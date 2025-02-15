66a08e7347af3887a70120834a83730fe9dbee5a
Ken Kunz <kennethkunz@gmail.com>
2025-02-15 17:35:35 -0600

    add initial grid of numbers (not so scary yet)

--- src/lib/components/DataField.svelte
+++ src/lib/components/DataField.svelte
@@ -1,11 +1,24 @@
-<section>Scary numbers</section>
+<section>
+	{#each { length: 500 }}
+		<div>{Math.floor(Math.random() * 10)}</div>
+	{/each}
+</section>
 
 <style>
 	section {
 		margin-block: 0.75rem;
 		border-block: 4px solid currentColor;
+		display: flex;
+		flex-wrap: wrap;
+		overflow: hidden;
+		font-size: 1.25rem;
+		font-weight: 300;
+	}
+
+	div {
+		flex: 0 0 5rem;
+		height: 5rem;
 		display: grid;
 		place-content: center;
-		font-size: 2rem;
 	}
 </style>
