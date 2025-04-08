26e9f8cf57f9fe2083f4e1d4d2c74e3a07227ab3
Ken Kunz <kennethkunz@gmail.com>
2025-04-08 14:53:46 -0500

    add hex coords to Footer (dummy/static vals for now)

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,9 +1,33 @@
-<footer>Hex coordinates</footer>
+<script>
+	let coords = $state([]);
+
+	function getHexString() {
+		return Math.random().toString(16).slice(2, 8).padEnd(6, '0');
+	}
+
+	$effect(() => {
+		coords = [getHexString(), getHexString()].sort();
+	});
+</script>
+
+<footer>
+	{#each coords as coord}
+		<span>0x{coord}</span>
+	{/each}
+</footer>
 
 <style>
 	footer {
 		display: grid;
+		grid-template-columns: auto auto;
 		place-content: center;
 		font-size: 1.5rem;
+		font-weight: 500;
+		letter-spacing: 0.04em;
+
+		span:first-child::after {
+			content: ':';
+			margin-inline: 1rem;
+		}
 	}
 </style>
