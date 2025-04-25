5d5cdce72cb06ea4d35120921773b7884897253f
Ken Kunz <kennethkunz@gmail.com>
2025-04-24 23:30:53 -0500

    add header elements (file title, logo)

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,11 +1,37 @@
-<header>Header</header>
+<script>
+	import LumonLogo from '$lib/assets/lumon-logo.svg?raw';
+</script>
+
+<header>
+	<h1>Svelte Summit</h1>
+	{@html LumonLogo}
+</header>
 
 <style>
 	header {
 		border-bottom: var(--border);
+		padding-inline: 4rem;
 		display: grid;
-		place-content: center;
-		font-size: 4rem;
-		font-weight: bold;
+		grid-template-columns: 1fr auto;
+		align-items: center;
+
+		h1 {
+			border: var(--border);
+			border-right: none;
+			margin-right: -3rem;
+			translate: 0 -1.5px;
+			height: 100px;
+			display: grid;
+			align-items: center;
+			padding-inline: 0.5rem;
+			font-size: 4em;
+			white-space: nowrap;
+			overflow: hidden;
+		}
+
+		:global(svg) {
+			height: 160px;
+			fill: currentColor;
+		}
 	}
 </style>
